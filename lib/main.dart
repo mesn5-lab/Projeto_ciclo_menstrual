c272adfa18fce7235584fff09301963deac0c430
duda <mesn5@aluno.ifal.edu.br>
2026-04-20 11:04:41 -0300

    Segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:ciclo_menstrual/bibliotecaPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: BibliotecaPage(), debugShowCheckedModeBanner: false)kd);
+  runApp(MaterialApp(home: BibliotecaPage(), debugShowCheckedModeBanner: false));
 }
