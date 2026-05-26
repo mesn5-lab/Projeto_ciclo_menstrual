9713298d7cb4109e1823528d6d9332fa326f8dad
duda <mesn5@aluno.ifal.edu.br>
2026-05-26 11:57:24 -0300

    aula_26/05

---
+++ lib/domain/Condicao.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Condicao {
+  String titulo;
+  String imagem;
+  Widget telaDestino;
+
+  Condicao({
+    required this.titulo,
+    required this.imagem,
+    required this.telaDestino,
+  });
+}
+// tela: Tela()
