b41918aa037003a4630423584fb9273a3f521a36
Maria Eduarda <mesn5@aluno.ifal.edu.br>
2026-09-22 09:24:35 -0300

    tirei os comentários
    Removed commented-out code for the old API and updated the base URL for the new API.

--- lib/api/medicamento_api.dart
+++ lib/api/medicamento_api.dart
@@ -3,19 +3,6 @@ import 'package:dio/dio.dart';
 
 class MedicamentoApi {
   final dio = Dio();
-  /*final String baseUrl = 'https://api.fda.gov/drug/drugsfda.json';
-
-  findByName(String name) async {
-    late Medicamento medicamento;
-    final response = await dio.get('$baseUrl?search=openfda.brand_name:"$name"&limit=1');
-
-    if (response.statusCode == 200) {
-      medicamento = Medicamento.fromJson(response.data);
-    }
-
-    return medicamento;
-  }*/
-
   final String baseUrl = 'https://my-json-server.typicode.com/mesn5-lab/Fake_api';
 
   Future<List<Medicamento>> listarMedicamento() async {
