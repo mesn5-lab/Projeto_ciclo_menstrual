a1ed472237c4b78517e3db0083d8490580553d76
Raissinha-Almeida <ras53@aluno.ifal.edu.br>
2026-09-22 10:50:58 -0300

    #davy - em loginPage chamei o HomeFixed ao inves do HomePage

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -238,7 +238,7 @@ class _LoginPageState extends State<LoginPage> {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-          builder: (context) => HomePage(),
+          builder: (context) => HomeFixed(),
         ),
       );
     } else {
